36dbcfcb73797b2f1e6fbe684fcf94861cdabd05
Robert <zimarobert0@gmail.com>
2023-10-24 20:11:38 +0200

    Creation of most of the layout, use of tailwind classes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["/public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background-color-1': '#EFE9E1',
+        'background-color-2': '#000000'
+      },
+      spacing: {
+        '3vh': '3vh',
+        '97vh': '97vh',
+        '200vh': '200vh'
+      }
+    },
   },
   plugins: [],
 }
